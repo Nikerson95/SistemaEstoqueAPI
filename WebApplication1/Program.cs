7c1fb8e2a1ccde08faa84835f776ef22f6af8d6d
Nikerson Ramos <nikersonvilela@hotmail.com>
2026-05-21 21:59:32 -0300

    atualizacao do codigo

--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -1,6 +1,7 @@
 using WebApplication1.DATA;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.SqlServer;
+using WebApplication1.Services.Autor;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,6 +10,21 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+builder.Services.// arquivo: WebApplication1/Services/Autor/IAutoService.cs (ou onde a classe está definida)
+namespace WebApplication1.Services.Autor
+{
+    public interface AutorInterface
+    {
+        // métodos da interface
+    }
+
+    // Corrigido: a classe agora implementa a interface AutorInterface
+    public class IAutoService : AutorInterface
+    {
+        // implementação dos métodos
+    }
+}AddScoped <AutorInterface, IAutoService>();
 
 builder.Services.AddDbContext<AppDbcontext>(options =>
 { 
